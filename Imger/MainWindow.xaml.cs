3ec2697c8b37aafcd9af23bad0c7fbbff8323efd
QingYi Studio <qingyi.studio@gmail.com>
2025-08-13 18:21:36 +0800

    Initial commit

---
+++ Imger/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Imger.Pages.OptimizationControlPanel;
+using System;
+using System.Reflection;
+using System.Windows;
+
+namespace Imger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainOcpPage mainOptimizationControlPanelPage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            UiInit();
+        }
+
+        private void UiInit()
+        {
+            //MainW.Title = MainW.Title + $" - Version {versionAttribute.InformationalVersion}";
+            mainOptimizationControlPanelPage = new MainOcpPage();
+            mainOptimizationControlPanelPage.InitializeComponent();
+        }
+
+        private void PreviewRendering_Drop(object sender, DragEventArgs e)
+        {
+            // Get the file data and render it in PreviewRendering control
+        }
+    }
+}
